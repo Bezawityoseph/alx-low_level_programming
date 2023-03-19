cad03a2b3e9d2622778c5dbeb478080fbefa8310
Bezawit <schoolbezawityoseph@yahoo.com>
2023-03-19 15:01:17 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,18 @@ int main(void)
 	int i;
 	int j;
 
-	for (i = 00 ; i <= 98 ; i++)
+	for (i = 0 ; i <= 98 ; i++)
 	{
-		for (j = 01 ; j <= 99 ; j++)
+		for (j = i + 1 ; j <= 99 ; j++)
 		{
-			putchar(i + '0');
-			putchar(j);
+			putchar((i / 10) + '0');
+			putchar((j % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((i % 10) + '0');
 			{
-				if (i < j && i != j)
+				if (i == 98 && j == 99)
+				continue;
 				{
 					putchar(',');
 					putchar(' ');
