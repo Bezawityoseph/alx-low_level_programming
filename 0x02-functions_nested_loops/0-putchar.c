293cf5042d524832630db6135b2c4fc6b782e8cf
Bezawit <schoolbezawityoseph@yahoo.com>
2023-03-22 09:33:57 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * main - main function
+ * main - Entry point
  *
  * Return: always 0
  */
 
 int main(void)
 {
-	putchar(_);
-	putchar(p);
-	putchar(u);
-	putchar(t);
-	putchar(c);
-	putchar(h);
-	putchar(a);
-	putchar(r);
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	putchar('\n');
 	return (0);
 }
