371980080192b3af4fcce29f5910745da431e100
Bezawit <schoolbezawityoseph@yahoo.com>
2023-03-31 05:13:32 -0700

    task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -4,18 +4,18 @@
  * more_numbers - print more numbers
  */
 
-int more_numbers(void)
+void more_numbers(void)
 {
 	int i, j;
-	
+
 	for (i = 1; i <= 10; i++)
-        {
-		for (j = 1; j <= 10; j++)
+	{
+		for (j = 0; j <= 14; j++)
 		{
-			if(j >= 10)
+			if (j >= 10)
 				_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-	       	_putchar('\n');
+		_putchar('\n');
 	}
 }
